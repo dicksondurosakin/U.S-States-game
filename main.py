1c9a9d18a6708340b39e54b2099494e094c3424d
Dickson Durosakin <durosakindickson@gmail.com>
2021-03-12 16:29:49 +0100

    i have made a change to this file now it tells you states you need to go and learn

--- main.py
+++ main.py
@@ -15,6 +15,8 @@ my_turtle.shape(image)
 us_state = pandas.read_csv("50_states.csv")
 states_dict = us_state.to_dict()
 
+correct_states = []
+learn_states = []
 score = 0
 Question = True
 while Question:
@@ -23,6 +25,16 @@ while Question:
         user_guess = my_screen.textinput("Guess the States", "Guess a State in the U.S").title()
     else:
         user_guess = my_screen.textinput(f"{score}/50 States Correct", "Guess a State in the U.S").title()
+
+    # This ends the game anytime the user press exit and make a csv file of states to learn
+    if user_guess == "Exit":
+        for i in range(len(states_dict["state"])):
+            if states_dict["state"][i] not in correct_states:
+                learn_states.append(states_dict["state"][i])
+        new_data_frame = pandas.DataFrame(learn_states)
+        new_data_frame.to_csv("states_to_learn.csv")
+        break
+
     xcor = ""
     ycor = ""
     # This part checks if the question is in the dictionary, writes it on the screen if it is
@@ -33,6 +45,8 @@ while Question:
             xcor = states_dict["x"][i]
             ycor = states_dict["y"][i]
             write = WritingTurtle(xcor, ycor, user_guess)
+            correct_states.append(user_guess)
 
-my_screen.exitonclick()
-
+    # This makes sure the while loop ends after the user has gotten all the states
+    if score == 51:
+        Question = False
